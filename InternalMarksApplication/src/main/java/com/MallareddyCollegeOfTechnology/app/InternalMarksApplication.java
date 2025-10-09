d92510d57ebe762f0a5832aa2b9427090b40d107
Roshan21424 <roshanraj21042004@gmail.com>
2025-10-10 04:15:37 +0530

    Add backend folder

---
+++ InternalMarksApplication/src/main/java/com/MallareddyCollegeOfTechnology/app/InternalMarksApplication.java
@@ -0,0 +1,14 @@
+package com.MallareddyCollegeOfTechnology.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalMarksApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(InternalMarksApplication.class, args);
+	}
+
+}
+
+
---
+++ InternalMarksApplication/src/test/java/com/MallareddyCollegeOfTechnology/app/InternalMarksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MallareddyCollegeOfTechnology.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalMarksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
